ce94cd64e0a1a20d382ba6802ab0d076b91d1891
Sam May <samrmay@sas.upenn.edu>
2021-06-10 14:50:28 -0400

    add function to calculate time density and u mtx

--- afssh.py
+++ afssh.py
@@ -1,8 +1,14 @@
 import numpy as np
 import random as rand
+import scipy.linalg as linalg
 
 
 class AFSSH():
+    """
+    Class to carry out Augmented Fewest Switches Surface Hopping algorithm. 
+    Closely follows http://dx.doi.org/10.1021/acs.jctc.6b00673 and equation references are from this paper
+    """
+
     def __init__(self, model, r0, v0, dt_c, dt_q, e_tol=1e-6, coeff=None, mass=2000, t0=0, state0=0, seed=None):
         self.model = model
         self.m = mass
@@ -73,11 +79,48 @@ class AFSSH():
 
         return r, v, a
 
+    def calc_overlap_mtx(self, r0, r1, correction=1e-6):
+        # Calculate phi(t0) and phi(t0 + tau)
+        ev0 = self.model.get_wave_functions(r0)
+        ev1 = self.model.get_wave_functions(r1)
+
+        # Calculate u_mtx, ensuring positive diagonal (eq. 5)
+        def calc(ev0, ev1):
+            u_mtx = np.zeros((self.num_states, self.num_states))
+            for i in range(self.num_states):
+                for j in range(self.num_states):
+                    u_mtx[i, j] = ev0[:, i]@ev1[:, j]
+            return u_mtx
+        u_mtx = calc(ev0, ev1)
+
+        if np.any(np.less(np.diag(u_mtx), np.zeros(self.num_states))):
+            u_mtx = calc(-1*ev0, ev1)
+
+        # Check for trivial crossing edge case and correct if necessary (eq. 30-33)
+        if np.any(np.equal(u_mtx, np.zeros(self.num_states))):
+            adj_ev0 = self.model.get_wave_functions(r0, correction=correction)
+            adj_ev1 = self.model.get_wave_functions(r1, correction=correction)
+            print("0 IN U_MTX. UNHANDLED CASE")
+            # ADD CORRECTION HERE
+
+        # Orthogonalize U (eq. 34) (Check if exponent is element wise)
+        u_mtx = u_mtx@((np.transpose(u_mtx)@u_mtx)**(-1/2))
+
+        return u_mtx
+
+    def calc_t_mtx(self, u_mtx, d_tc):
+        return (1/d_tc)*linalg.logm(u_mtx)
+
     def step(self):
-        # Nuclear classical evolution
+        # Initialize time steps (may change depending on energy conservation)
+        dt_c = self.dt_c
+
+        # Nuclear classical evolution using classical time step dt_c
         r0 = self.r
         v0 = self.v
         a0 = self.a
-        r, v, a = self.calc_traj(r0, v0, self.dt_c, self.m, a0=a0)
+        r, v, a = self.calc_traj(r0, v0, dt_c, self.m, a0=a0)
 
-        # Calculate overlap matrix
+        # Calculate overlap matrix and time density mtx
+        u_mtx = self.calc_overlap_mtx(dt_c)
+        t_mid = self.calc_t_mtx(u_mtx, dt_c)
