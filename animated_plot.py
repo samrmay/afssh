420024d6a03e037ecf005b132bd3b8b030417062
Sam May <samrmay@sas.upenn.edu>
2021-06-12 12:45:34 -0400

    fix animation to work with afssh

--- animated_plot.py
+++ animated_plot.py
@@ -4,22 +4,24 @@ import stopping_functions as fcns
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+f = fcns.basic_1d(-6, 6)
 
-def animate_1d(fssh, fig, ax, stopping_function=fcns.f(-6, 6), min_x=-10, max_x=10, interval=50, num_points=400):
+
+def animate_1d(fssh, fig, ax, stopping_function=f, min_x=-10, max_x=10, interval=50, num_points=400):
     x_linspace = np.linspace(min_x, max_x, num_points)
     model = fssh.model
     models.plot_1d(ax, model, x_linspace)
 
-    point, = ax.plot(fssh.r, model.get_adiabatic_energy(
-        fssh.r)[fssh.e_state], 'ro')
-    v_text = plt.text(-5, 0, fssh.v)
+    point, = ax.plot(fssh.r[0], model.get_adiabatic_energy(
+        fssh.r[0])[fssh.lam], 'ro')
+    v_text = plt.text(-5, 0, fssh.v[0])
 
     def animate(_):
-        if not fssh.step(stopping_function):
+        if not fssh.step(fssh.dt_c):
             return None
-        point.set_xdata(fssh.r)
-        point.set_ydata(model.get_adiabatic_energy(fssh.r)[fssh.e_state])
-        v_text.set_text(fssh.v)
+        point.set_xdata(fssh.r[0])
+        point.set_ydata(model.get_adiabatic_energy(fssh.r[0])[fssh.lam])
+        v_text.set_text(fssh.v[0])
         return point, v_text,
 
     ani = animation.FuncAnimation(
@@ -27,19 +29,19 @@ def animate_1d(fssh, fig, ax, stopping_function=fcns.f(-6, 6), min_x=-10, max_x=
     return ani
 
 
-def animate_2d(fssh, fig, ax, x_linspace, y_linspace, stopping_function=fcns.f(-6, 6), interval=50, num_points=400, colors=["black", "red"]):
+def animate_2d(fssh, fig, ax, x_linspace, y_linspace, stopping_function=f, interval=50, num_points=400, colors=["black", "red"]):
     model = fssh.model
     models.plot_2d(ax, model, x_linspace, y_linspace, colors)
 
     point, = ax.plot(fssh.r[0], fssh.r[1], model.get_adiabatic_energy(
-        fssh.r)[fssh.e_state], marker='o')
+        fssh.r)[fssh.lam], marker='o')
 
     def animate(_):
-        if not fssh.step(stopping_function):
+        if not fssh.step(fssh.dt_c):
             return None
         point.set_data(fssh.r)
         point.set_3d_properties(
-            model.get_adiabatic_energy(fssh.r)[fssh.e_state])
+            model.get_adiabatic_energy(fssh.r)[fssh.lam])
         return point,
 
     ani = animation.FuncAnimation(
