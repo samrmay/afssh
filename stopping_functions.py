3af05f5082201a2f24c13bebac8512e397956274
Sam May <samrmay@sas.upenn.edu>
2021-06-12 10:54:56 -0400

    add script for returing stopping functions

---
+++ stopping_functions.py
@@ -0,0 +1,9 @@
+def basic_1d(left, right):
+    def f(fssh):
+        if fssh.r[0] > right and fssh.v[0] >= 0:
+            return True
+        elif fssh.r[0] < -left and fssh.v[0] <= 0:
+            return True
+        return False
+    
+    return f
