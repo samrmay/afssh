c12ff42b0a66aec3a6dd5b05cd5e532fead72c6f
Sam May <samrmay@sas.upenn.edu>
2021-06-13 20:25:37 -0400

    fix negative sign on elif basic_1d

--- stopping_functions.py
+++ stopping_functions.py
@@ -2,7 +2,7 @@ def basic_1d(left, right):
     def f(fssh):
         if fssh.r[0] > right and fssh.v[0] >= 0:
             return True
-        elif fssh.r[0] < -left and fssh.v[0] <= 0:
+        elif fssh.r[0] < left and fssh.v[0] <= 0:
             return True
         return False
     
